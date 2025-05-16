c81659929378c0517553c20d1b9ecec7882df89c
git <school@email>
2025-05-15 17:26:32 -0700

    Updated Navbar Layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ export const Navbar = ({ menuOpen, setMenuOpen}) => {
         document.body.style.overflow = menuOpen ? "hidden" : "";
     }, [menuOpen])
     return (
-      <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
-        <div className="max-w-5xl mx-auto px-4">
-          <div className="flex justify-between items-center h-16">
+      <nav className="fixed top-0 w-full z-40 bg-pink-100 border-b border-white/10 shadow-lg">
+        <div className="mx-auto px-4">
+          <div className="flex items-center justify-between h-12">
             <a href="#home" className="font-mono text-xl font-bold text-black">
-              Jaycee<span className="text-pink-500">.tech</span>
+              Jaycee<span className="text-rose-400">.tech</span>
             </a>
             <div
               className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
@@ -17,28 +17,28 @@ export const Navbar = ({ menuOpen, setMenuOpen}) => {
             >
               &#9776;
             </div>
-            <div className="hidden md:flex items-center space-x-8">
+            <div className="hidden md:flex items-center space-x-15 font-bold text-md">
               <a
                 href="#home"
-                className="text-pink-400 hover-text-white transition-colors"
+                className="text-rose-400 hover:text-rose-600 transition-colors"
               >
                 Home
               </a>
               <a
-                href="#About"
-                className="text-pink-400 hover-text-white transition-colors"
+                href="#about"
+                className="text-rose-400 hover:text-rose-600 transition-colors"
               >
                 About
               </a>
               <a
-                href="#Projects"
-                className="text-pink-400 hover-text-white transition-colors"
+                href="#projects"
+                className="text-rose-400 hover:text-rose-600 transition-colors"
               >
                 Projects
               </a>
               <a
-                href="#Contact"
-                className="text-pink-400 hover-text-white transition-colors"
+                href="#contact"
+                className="text-rose-400 hover:text-rose-600 transition-colors"
               >
                 Contact
               </a>
--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -15,19 +15,21 @@ export const About = () => {
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
               About Me
             </h2>
-            <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-              <p className="text-gray-300 mb-6">
+            <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all  hover:bg-pink-200/50">
+              <p className="text-black mb-6">
                 I am passionate blah blah good solutions yipee
               </p>
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                  <h3 className="text-xl font-bold mb-4">Frontend</h3>
+                  <h3 className="text-xl font-bold text-black mb-4">
+                    Frontend
+                  </h3>
                   <div className="flex flex-wrap gap-2">
                     {frontendSkills.map((tech, key) => (
                       <span
                         key={key}
-                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition"
+                        className="bg-rose-500/10 text-rose-600 py-1 px-3 rounded-full text-sm hover:bg-rose-400/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition"
                       >
                         {tech}
                       </span>
@@ -36,7 +38,7 @@ export const About = () => {
                 </div>
 
                 <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                  <h3 className="text-xl font-bold mb-4">Backend</h3>
+                  <h3 className="text-xl font-bold text-black mb-4">Backend</h3>
                   <div className="flex flex-wrap gap-2">
                     {backendSkills.map((tech, key) => (
                       <span
