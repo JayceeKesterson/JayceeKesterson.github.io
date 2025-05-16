0a7b410dad72ed6709d745308330d411b290fd42
JayceeKesterson <144744389+JayceeKesterson@users.noreply.github.com>
2025-05-16 05:38:42 +0000

    Added styling and colors

--- src/components/sections/Contacts.jsx
+++ src/components/sections/Contacts.jsx
@@ -1,34 +1,34 @@
 import { useState } from "react";
 import { RevealOnScroll } from "../RevealOnScroll";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 
 export const Contact = () => {
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        message: "",
-    });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-    const SERVICE_ID = "service_eyc4ilq";
-    const TEMPLATE_ID = "template_2lmuuka";
-    const PUBLIC_KEY = "xI3-mJ0iFCvW96yLb";
+  const SERVICE_ID = "service_eyc4ilq";
+  const TEMPLATE_ID = "template_2lmuuka";
+  const PUBLIC_KEY = "xI3-mJ0iFCvW96yLb";
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        emailjs
-          .sendForm(
-            import.meta.env.VITE_SERVICE_ID,
-            import.meta.env.VITE_TEMPLATE_ID,
-            e.target,
-            import.meta.env.VITE_PUBLIC_KEY
-          )
-          .then((result) => {
-            alert("Message Sent!");
-            setFormData({ name: "", email: "", message: "" });
-          })
-          .catch(() => alert("Oops! Something went wrong. Please try again."));
-    };
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then((result) => {
+        alert("Message Sent!");
+        setFormData({ name: "", email: "", message: "" });
+      })
+      .catch(() => alert("Oops! Something went wrong. Please try again."));
+  };
 
   return (
     <section
@@ -37,7 +37,7 @@ export const Contact = () => {
     >
       <RevealOnScroll>
         <div className="px-4 w-150">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-pink-300 to-rose-400 bg-clip-text text-transparent leading-right text-center cursor-default">
             Get In Touch
           </h2>
           <form className="space-y-6" onSubmit={handleSubmit}>
@@ -48,9 +48,11 @@ export const Contact = () => {
                 name="name"
                 required
                 value={formData.name}
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                              placeholder="Name..."
-                              onChange={(e) => setFormData({...formData, name: e.target.value})}
+                className="w-full bg-white/5 border  border-rose-400 text-rose-400 rounded px-4 py-3 text-black transition focus:outline-none focus:bg-rose-500/10"
+                placeholder="Name..."
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
               />
             </div>
             <div className="relative">
@@ -60,9 +62,11 @@ export const Contact = () => {
                 name="email"
                 required
                 value={formData.email}
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                              placeholder="example@gmail.com"
-                                onChange={(e) => setFormData({...formData, email: e.target.value})}
+                className="w-full bg-white/5 border  border-rose-400 text-rose-400 rounded px-4 py-3 text-black transition focus:outline-none focus:bg-rose-500/10"
+                placeholder="example@gmail.com"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
               />
             </div>
             <div className="relative">
@@ -72,12 +76,19 @@ export const Contact = () => {
                 required
                 value={formData.message}
                 rows={5}
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                              placeholder="Your Message..."
-                                onChange={(e) => setFormData({...formData, message: e.target.value})}
+                className="w-full bg-white/5 border border-rose-400 text-rose-400 rounded px-4 py-3 text-black transition focus:outline-none focus:bg-rose-500/10 max-h-40 min-h-40 resize-none"
+                placeholder="Your Message..."
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
               />
-                      </div>
-                      <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow[0_0_15px+rgba(59, 130, 246, 0.4)]">Send Message</button>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-rose-400 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5  hover:shadow-xl"
+            >
+              Send Message
+            </button>
           </form>
         </div>
       </RevealOnScroll>
